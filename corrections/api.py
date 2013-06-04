4340ee74d53369e5ebf9464102fca82511856215
plausibility <chris@gibsonsec.org>
2013-06-04 18:30:36 +1000

    Here we go, this does nothing but it's set up.

---
+++ corrections/api.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+import twitter
+
+class Bot(object):
+    pass
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+import corrections
+
+
+def long_desc():
+    with open('README.rst', 'rb') as f:
+        return f.read()
+
+kw = {
+    "name": "corrections",
+    "version": corrections.__version__,
+    "description": '',
+    "long_description": long_desc(),
+    "url": "https://github.com/plausibility/corrections",
+    "author": "plausibility",
+    "author_email": "chris@gibsonsec.org",
+    "license": "MIT",
+    "packages": [
+        'corrections'
+    ],
+    "package_dir": {
+        "corrections": "corrections"
+    },
+    "install_requires": [
+        "gevent",
+        "twitter"
+    ],
+    "zip_safe": False,
+    "keywords": "twitter bot corrections",
+    "classifiers": [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2"
+    ]
+}
+
+if __name__ == "__main__":
+    setup(**kw)
