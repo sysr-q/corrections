e9d00447908418925ec9f00a15a727535f82451e
plausibility <chris@gibsonsec.org>
2013-06-18 23:49:13 +1000

    Now we have a functional cooldown/queue controller.

--- corrections/__init__.py
+++ corrections/__init__.py
@@ -16,13 +16,20 @@ __version__ = "0.1.0"
 
 
 class Correct(object):
-    def __init__(self, auth, phrases=None, cooldown=20):
+    def __init__(self,
+                 auth,
+                 phrases=None,
+                 cooldown=(30, 10),
+                 force_fetch=False):
         """ Create a new correctional bot, with a Twitter instance ready.
 
             :auth: a tuple/list with the information from Twitter:
                 (OAUTH_TOKEN, OUTH_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
             :phrases: an iterable (or string) of phrases to search for.
-            :cooldown: how long to wait (minimum) between searches
+            :cooldown: how long to wait in seconds between searching for
+                new tweets, and sending off tweets; as a tuple, like (20, 30)
+            :force_fetch: should we fetch new tweets, even if we haven't
+                fully drained our tweet queue?
         """
         self.t = twitter.Twitter(auth=twitter.OAuth(*auth))
         try:
@@ -36,12 +43,14 @@ class Correct(object):
             self.phrases = phrases
         # We're going to want strings anyway, so..
         self.phrases = map(str, self.phrases)
-        self.cooldown = cooldown
+        self._cooldown_fetch, self._cooldown_tweet = cooldown
+        self.force_fetch = force_fetch
 
         self.queue = Queue()
         self.producer = gevent.spawn(self._produce)
         self.seen = []
-        self.next = time.time()
+        self._next_fetch = time.time()
+        self._next_tweet = time.time()
 
     #def _add_work(self, amt=50):
     #    [self.queue.put_nowait(i) for i in xrange(amt)]
@@ -51,15 +60,17 @@ class Correct(object):
         for t in tweets['statuses']:
             if t['id'] in self.seen:
                 continue
+            self.seen.append(t['id'])
+            print "Got:", t['id']   # /debug
             self.queue.put_nowait(t)
-        self.next = time.time() + self.cooldown
+        self._next_fetch = time.time() + self._cooldown_fetch
 
     def _produce(self):
         while True:
-            if not self.queue.empty():
+            if not self.force_fetch and not self.queue.empty():
                 gevent.sleep(0)
                 continue
-            if self.next > time.time():
+            if self._next_fetch > time.time():
                 gevent.sleep(0)
                 continue
             self._fetch_tweets()
@@ -69,11 +80,15 @@ class Correct(object):
         while True:
             if self.queue.empty():
                 gevent.sleep(0)
+            if self._next_tweet > time.time():
+                gevent.sleep(0)
+                continue
             task = self.queue.get()
             print u"Should be replying to: {0} (from @{1})".format(
                 task['text'],
                 task['user']['screen_name']
-            )
+            )  # /debug
+            self._next_tweet = time.time() + self._cooldown_tweet
     __call__ = do_loop
 
     def reply(self, phrase, user):
